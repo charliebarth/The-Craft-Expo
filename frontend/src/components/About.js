226b2d40ca01105c0b64e8a47b543a8cfde75c09
github username <charliepbarth@gmail.com>
2021-04-07 17:33:52 -0400

    Added a div id to about component

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,7 +1,7 @@
 export default function About() {
 
     return (
-      <>
+      <div id="about">
         <h2>About</h2>
   
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt quam sed urna ornare, et interdum justo consequat. Maecenas ut risus id orci faucibus fringilla. Nulla sagittis a neque vitae faucibus. Praesent augue lacus, fringilla sit amet molestie at, fringilla ac felis. Duis tincidunt urna a finibus varius. Curabitur metus elit, efficitur quis fermentum at, imperdiet vel ipsum. Morbi ligula diam, mollis a erat ut, finibus dictum ligula.</p>
@@ -13,7 +13,7 @@ export default function About() {
         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi imperdiet sodales orci quis vulputate. Quisque ac laoreet enim, ac commodo lorem. Aenean faucibus interdum nisi, ut imperdiet ligula bibendum non. Nunc et interdum diam, a sollicitudin risus. Pellentesque nec iaculis turpis. Sed eget sagittis sapien. Nulla nec massa vel velit commodo egestas. Vestibulum cursus nibh et turpis fringilla, molestie posuere tellus placerat. Cras mollis pretium molestie. Duis dictum ipsum eget leo condimentum ultrices.</p>
   
         <p>Integer sit amet cursus ex, dictum auctor sem. Nunc ultricies sit amet risus et faucibus. Integer pretium ullamcorper quam, non mollis lectus maximus in. Vestibulum turpis justo, rutrum in lacus sit amet, iaculis imperdiet eros. Integer iaculis id est nec vehicula. Nulla ultricies diam felis, vitae finibus nibh suscipit vel. Proin condimentum quis nulla ac sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer laoreet, justo nec semper eleifend, metus purus blandit nibh, a fermentum nibh est sollicitudin augue. Maecenas non enim et odio vulputate sollicitudin. Aenean id lectus in felis finibus imperdiet sit amet ac lacus. Phasellus maximus mi at nulla tristique, pellentesque iaculis urna placerat. Donec consequat eleifend suscipit. Mauris et pulvinar lacus. </p>
-      </>
+      </div>
     )
   
   }
