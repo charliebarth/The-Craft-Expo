da6f012f92f1766ca4a918cda8b460c278f8cc89
github username <charliepbarth@gmail.com>
2021-04-07 16:41:11 -0400

    Cleaned up whitespaces and commented out code

--- frontend/src/components/DraftingBoard.js
+++ frontend/src/components/DraftingBoard.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom'
 export default function CraftList({crafts}) {
 
   const renderCrafts = () => {
-    console.log(Array.isArray(crafts.data))
     return crafts.data.map(c => <Link to={`/crafts/${c.attributes.name.toLowerCase().replace(/[ ]/g, '-')}`}>{c.attributes.name}</Link>)
   }
 
   return (
-
+    
     <div id="crafts-list">
 
       <div/>
@@ -20,7 +19,6 @@ export default function CraftList({crafts}) {
       {renderCrafts()}
 
     </div>
-
   )
 
 }
