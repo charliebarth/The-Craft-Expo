12d97f62983c0700d5af9b3405ab6eb61e3a3e1c
github username <charliepbarth@gmail.com>
2021-04-06 20:24:09 -0400

    Added initial draft of crafts display page

---
+++ frontend/src/components/DraftingBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Crafts = ({ title, img_url }) => (
+  <div className="craft">
+    {/* <img src={ img_url } alt={title}/>
+    <h3>{ title }</h3> */}
+    <p>Test</p>
+  </div>
+)
+
+export default Crafts; 
