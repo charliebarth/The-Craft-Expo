41176166f223284b73a59505be40cfbe4cb974e3
github username <charliepbarth@gmail.com>
2021-04-09 21:38:27 -0400

    Rolled back data base and changed user atrributes

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,28 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_182206) do
-
-  create_table "crafts", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "img_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "user_crafts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "craft_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
