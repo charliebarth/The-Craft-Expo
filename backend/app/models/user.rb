776bb916e05429082b2ee0e599b7dad8ca7c0262
github username <charliepbarth@gmail.com>
2021-04-09 21:31:04 -0400

    Added user validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+    validates_uniqueness_of :username
+    validate_presence_of :password_digest
     
     has_many :user_crafts
     has_many :crafts, through: :user_crafts
