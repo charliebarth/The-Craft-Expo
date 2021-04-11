21a38ede4514d0d402ea8a6dc4c50b9263a796e0
github username <charliepbarth@gmail.com>
2021-04-10 21:01:13 -0400

    added attributes to user_craft migration

--- backend/db/migrate/20210331182206_create_user_crafts.rb
+++ backend/db/migrate/20210331182206_create_user_crafts.rb
@@ -2,7 +2,9 @@ class CreateUserCrafts < ActiveRecord::Migration[6.0]
   def change
     create_table :user_crafts do |t|
       t.integer :user_id
-      t.integer :craft_id
+      t.string :name
+      t.string :description
+      t.string :img_url
 
       t.timestamps
     end
