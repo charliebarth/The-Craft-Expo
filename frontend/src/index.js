041b04bd7df567b2f9d16bec7bae92e84df60a26
github username <charliepbarth@gmail.com>
2021-04-11 00:36:27 -0400

    Updated Index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store} >
     <App />
-  </React.StrictMode>,
+    </Provider >,
   document.getElementById('root')
 );
 
