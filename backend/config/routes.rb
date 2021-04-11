b1e096a1bc05aaf61095c0048c8e83dbbd606008
github username <charliepbarth@gmail.com>
2021-04-11 00:32:56 -0400

    Updated routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :demos, only: [:index]
   resources :crafts, only: [:create, :destory, :index]
-  resources :users, [:create]
+  resources :users, only: [:create]
+  resources :auth, only: [:create]
 
   get 'profile', to: "auth#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
